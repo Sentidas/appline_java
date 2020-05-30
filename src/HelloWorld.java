e819fd8594906fb815501e25c8066ae648284a8c
Elena Bakhareva <sentidas@yandex.ru>
2020-05-31 00:54:57 +0300

    do task 3

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+// IntelliJ IDEA установлена
+// репозиторий на GitHube создан
+
+
+// проект необходимо отправлять со всеми файлами? 
+
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
